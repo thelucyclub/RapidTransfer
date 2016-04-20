851ec6d28529fe81de0a2f4724a826e4d52f2a80
BuildTools <unconfigured@null.spigotmc.org>
2016-04-20 13:59:08 -0400

    :circus_tent: DMCA Takedown From PocketMine\PocketMine-MP :circus_tent:

--- bin/x/j/src/pocketmine/item/EnchantingBottle.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace pocketmine\item;
-
-class EnchantingBottle extends Item{
-	public function __construct($meta = 0, $count = 1){
-		parent::__construct(self::ENCHANTING_BOTTLE, $meta, $count, "Bottle o' Enchanting");
-	}
-}
